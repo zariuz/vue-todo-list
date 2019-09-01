f76b8416d3e6a3e05aec859dff92c43eabf2e9b1
zariuz <nechetov@hotmail.com>
2019-09-01 17:34:57 +0300

    project creation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  h1 Hello
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     HelloWorld
   }
@@ -17,12 +14,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
