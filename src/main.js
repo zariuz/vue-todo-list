28c62c77b72091b56833ef5d265cbc9da998d632
zariuz <nechetov@hotmail.com>
2019-09-08 21:00:36 +0300

    add vuex and state

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import SimpleVueValidation from 'simple-vue-validator';
 import router from './router';
+import { store } from './store';
 
 Vue.use(SimpleVueValidation);
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app');
---
+++ src/store/modules/todos.js
@@ -0,0 +1,8 @@
+const todos = {
+  state: {},
+  actions: {},
+  getters: {},
+  mutations: {}
+};
+
+export default todos;
